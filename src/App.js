b50cf6f425b44adac31006ad76f54ec86b7c793f
Thepnathi Stephenson <Thepnathi@hotmail.com>
2018-03-29 22:24:47 +0100

    Added swipeable nav

--- src/App.js
+++ src/App.js
@@ -1,27 +1,78 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 // TODO: make sure to remove later // We may not need this because swipeable
-// NOTE:
-// IDEA:
 import SwipeableViews from 'react-swipeable-views';
 import MyNavbar from './components/my-navbar';
 import Home from './components/home'
+import Projects from './components/project'
 import Contacts from './components/contacts';
 import './App.css';
 
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
+  },
+  slide: {
+    padding: 10,
+  },
+};
+
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      slideIndex: 0,
+    };
+  }
+
+  handleChange = (value) => {
+    this.setState({
+      slideIndex: value,
+    });
+  };
+
   render() {
     return (
-      <Router>
+      <MuiThemeProvider>
+      <Tabs
+        onChange={this.handleChange}
+        value={this.state.slideIndex}
+      >
+        <Tab label="Home" value={0} />
+        <Tab label="Project" value={1} />
+        <Tab label="Blogs" value={2} />
+        <Tab label="Contacts" value={3} />
+        <Tab label="Github" value={4} />
+      </Tabs>
+      <SwipeableViews
+        index={this.state.slideIndex}
+        onChangeIndex={this.handleChange}
+      >
         <div>
-          <MyNavbar />
-
-
-
-          <Route exact path="/" component={Home} />
-          <Route path="/contacts" component={Contacts} />
+          <Home />
         </div>
-      </Router>
+        <div style={styles.slide}>
+          <Projects />
+        </div>
+        <div style={styles.slide}>
+          3
+        </div>
+        <div style={styles.slide}>
+          4
+        </div>
+        <div style={styles.slide}>
+          5
+        </div>
+      </SwipeableViews>
+    </MuiThemeProvider>
     );
   }
 }
