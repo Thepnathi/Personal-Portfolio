be79a0638bbb068cd0806a868690ac00cf9d8e72
Thepnathi Stephenson <Thepnathi@hotmail.com>
2018-04-06 19:38:03 +0100

    add github calendar

---
+++ src/components/github-calendar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './about-me.css';
+
+export default class Github-Calendar extends Component {
+  render() {
+    return (
+      <div>
+        <div className="calendar">
+      </div>
+    )
+  }
+}
