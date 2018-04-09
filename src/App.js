e6ab007baffdba2f02d306d903b6d0bbf154f344
Thepnathi Stephenson <Thepnathi@hotmail.com>
2018-04-09 10:32:26 +0100

    update github-calendar

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from './components/home';
 import AboutMe from './components/about-me';
 import Projects from './components/projects';
 import Contacts from './components/contacts';
+import GitHubCalendar from './components/github-calendar';
 import './App.css';
 import './components/stylesheet/my-theme.css';
 
@@ -15,15 +16,18 @@ import './components/stylesheet/my-theme.css';
 export default class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <MyNavbar />
-          <Route exact path="/" component={Home}></Route>
-          <Route  path="/about-me" component={AboutMe}></Route>
-          <Route path="/projects" component={Projects}></Route>
-          <Route path="/blog" component={Blog}></Route>
-        </div>
-      </Router>
+      <div>
+        <GitHubCalendar />
+      </div>
+      // <Router>
+      //   <div>
+      //     <MyNavbar />
+      //     <Route exact path="/" component={Home}></Route>
+      //     <Route  path="/about-me" component={AboutMe}></Route>
+      //     <Route path="/projects" component={Projects}></Route>
+      //     <Route path="/blog" component={Blog}></Route>
+      //   </div>
+      // </Router>
     );
   }
 }
---
+++ src/components/github-calendar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./stylesheet/github-calendar.css";
+
+export default class GitHubCalendar extends Component {
+  render() {
+    return (
+      <div>
+        <div className="calendar"></div>
+      </div>
+    );
+  }
+}
