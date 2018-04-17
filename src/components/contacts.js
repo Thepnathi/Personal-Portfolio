a5bbb8e8ac6686dc7271a322483ff550b148c439
Thepnathi Stephenson <Thepnathi@hotmail.com>
2018-04-17 17:27:25 +0100

    update contact component

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -2,28 +2,46 @@ import React, { Component } from 'react';
 import './stylesheet/contact.css';
 
 export default class Contacts extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {term : ''};
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
+
   render() {
     return (
       <div className="contact-wrapper">
           <div>
             <form>
                 Your Name:
-                <input type="text"></input>
-
+                <input type="text" ></input>
+                <br />
+                Your Email:
+                <input type="text" ></input>
             </form>
           </div>
 
           <div>
-
+            <form>
+              <input
+                type="text"
+                className="email-message"
+                placeholder="Enter your message here..."
+                onChnage = {this.onInputChange}>
+              </input>
+            </form>
 
           </div>
+
+          <button>djdjjdj</button>
+
       </div>
     );
   }
 }
-{/* <form>
-  First name:<br>
-  <input type="text" name="firstname"><br>
-  Last name:<br>
-  <input type="text" name="lastname">
-</form> */}
