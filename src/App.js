c1c47ad8142c7e710bc8cc1735b037957f7f1de8
Thepnathi <thepnathi@hotmail.com>
2018-05-18 22:39:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 // TODO: make sure to remove later // We may not need this because swipeable
 // Remove Material UI
+// Add my personal blog platoform API to this to update with latest feed
 import MyNavbar from './components/my-navbar';
 import Home from './components/home';
 import AboutMe from './components/about-me';
