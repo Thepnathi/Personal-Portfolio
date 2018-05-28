b70938efc163eea69e19694a2d6a972f94e0adc9
Thepnathi Stephenson <Thepnathi@hotmail.com>
2018-05-28 21:47:49 +0100

    tweak some style and change text on the profile

--- src/components/home.js
+++ src/components/home.js
@@ -13,9 +13,9 @@ export default class Home extends Component {
       <div>
           <div className="introduction">
             <div className="box1">
-              <h1 className="animated-title">Welcome Back.</h1>
-              <h2 className="intro-title">Self-Taught Full-Stack Developer</h2>
+              <h1 className="animated-title">Welcome to my page</h1>
               <h2 className="intro-title">Computer Science Student</h2>
+              <h2 className="intro-title">Full-Stack Developer</h2>
 
               <div className="social-network-logo">
                 <div><a href="#" className="dark-black"><i class="fab fa-github-square"></i></a></div>
@@ -26,17 +26,18 @@ export default class Home extends Component {
 
             </div>
 
-            <div className="box2 short-bio">
-              <img className="intro-parts" className="profile-image" src="../assets/professional-profile-image.jpg"  href="somepage"/>
-              <h2>About Me</h2>
-              <p>Current student at University of Liverpool. I started coding seriously in 2018.
-                I have dabble with varies kinds of development. When I am not studying
-                or developing. You can find me in the gym or practicing with the University's Archery Club.
-                <a href="#"> Check my blog to learn more about me.</a></p>
-            </div>
+             <div className="box2 short-bio">
+               <img className="intro-parts" className="profile-image" src="../assets/professional-profile-image.jpg"  href="somepage"/>
+               <h2>About Me</h2>
+               <p>Current student at University of Liverpool. I started coding seriously in 2018.
+                 I have dabble with varies kinds of development. When I am not studying
+                 or developing. You can find me in the gym or practicing with the University's Archery Club.
+                 Everytime I hear someone mention Java and Javascript are the same, I lose a year's worth of life expectency.
+                 <a href="#"> Check my blog to learn more about me.</a></p>
+             </div>
 
             <div className="bio-box box4 secondary-color-theme"><p className="intro-parts">
-              Computer Science student with with creativity and problem solving skills.</p></div>
+              Computer Science student with creativity and problem solving skills.</p></div>
             <div className="bio-box box3 secondary-color-theme">
               <p className="intro-parts">Currrently developing with the MERN stack
                (<mark class="mongo">MongoDB</mark>,
@@ -44,10 +45,11 @@ export default class Home extends Component {
                 <mark class="react">React.js</mark> and
                 <mark class="node">Node.js</mark>)</p></div>
             <div className="bio-box box5 secondary-color-theme"><p className="intro-parts">
-              Every projects built by me has a real life purpose. <a href="#">Want to read the funniest jokes?</a>.</p></div>
+              Every projects built by me has a real life purpose. <a href="#">Check them here</a>.</p></div>
             <div className="bio-box box6 secondary-color-theme"><p className="intro-parts">
-              Have an idea to change the world? Or want to create something fun?
-              <a componentClass={Link} href="/contacts" to="/contact"> Get in touch.</a></p></div>
+              Want to build something?
+              <a componentClass={Link} href="/contacts" to="/contact"> Get in touch. I love to build things.
+              Especially with others.</a></p></div>
           </div>
           <AboutMe />
           <Projects />
